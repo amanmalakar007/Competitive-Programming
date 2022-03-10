ce486789acedf0896ff5e621a379e93fd6b79a6b
Aman Malakar <amanmalakar007@gmail.com>
2022-03-10 23:34:52 +0530

    Added Leetcode 1805 soln.

---
+++ LeetCode/Number-of-Different-Integers-in-a-String-1805/Solution.java
@@ -0,0 +1,14 @@
+import java.math.BigDecimal;
+class Solution {
+    public int numDifferentIntegers(String word) {
+        String arr[] = word.split("[a-z]");
+	    Set<BigDecimal> mySet = new HashSet<BigDecimal>();
+	    for (int i = 0; i < arr.length; i++) {
+		    if (arr[i].isEmpty() == false) {
+			    BigDecimal bd = new BigDecimal(arr[i]);
+			    mySet.add(bd);
+		    }
+	    }
+	    return (mySet.size());
+    }
+}
