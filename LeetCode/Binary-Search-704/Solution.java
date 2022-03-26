7b7ff6939b64428943329048ab445dd6755ec1fc
Aman Malakar <amanmalakar007@gmail.com>
2022-03-26 23:25:58 +0530

    Added Leetcode 704 to repo

---
+++ LeetCode/Binary-Search-704/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int high = nums.length-1, low = 0;
+        int mid = (low+high)/2;
+        while(low<=high)
+        {
+            mid = (low+high)/2;
+            if(target == nums[mid])
+            {
+                return mid;
+            }
+            else if (target < nums[mid])
+            {
+                high = mid-1;
+            }
+            else
+            {
+                low = mid+1;
+            }
+        }
+        return -1;
+    }
+}
