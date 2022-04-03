d05f5ce929576d695c8d8498efca9bdc76889a2f
Aman Malakar <amanmalakar007@gmail.com>
2022-04-03 13:28:13 +0530

    Updated solution for Leetcode 680.
    Updated the solution for Q. 680 with my own solution (better than 75% java submissions)

--- LeetCode/680-Valid-Palindrome-II/Solution.java
+++ LeetCode/680-Valid-Palindrome-II/Solution.java
@@ -1,26 +1,31 @@
-//not my solution
-//source: https://leetcode.com/problems/valid-palindrome-ii/discuss/391809/Java-Solutions-to-Valid-Palindrome-I-and-II-with-Explanation-(SubPalindrome-Iteration-and-Recursion)
 class Solution {
+    int errors = 0;
     public boolean validPalindrome(String s) {
-  int n = s.length();
-  for (int i = 0; i < n / 2; ++i) {
-    int left = i, right = n - i - 1;
-    if (s.charAt(left) != s.charAt(right)) { // give a last chance
-      // delete char at left
-      if (validSubPalindrome(s, left + 1, right)) return true;
-      // delete char at right
-      return validSubPalindrome(s, left, right - 1);
+        int len = s.length();
+        int i, j;
+        for (i = 0, j = len - 1; i < len && j >= 0 && i <= j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
+                errors++;
+                if (checkPalindrome(s, i + 1, j, len))
+                    return true;
+                else if (checkPalindrome(s, i, j - 1, len))
+                    return true;
+                else
+                    return false;
+            }
+        }
+        if (errors <= 1)
+            return true;
+        return false;
     }
-  }
-  return true;
-}
 
-private boolean validSubPalindrome(String s, int lo, int hi) {
-  int n = hi - lo + 1;
-  for (int i = 0; i < n / 2; ++i) {
-    int left = lo + i, right = hi - i;
-    if (s.charAt(left) != s.charAt(right)) return false;
-  }
-  return true;
-}
+    private boolean checkPalindrome(String s, int i, int j, int len) {
+        for (; i < len && j >= 0 && i <= j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
+                errors++;
+                return false;
+            }
+        }
+        return true;
+    }
 }
