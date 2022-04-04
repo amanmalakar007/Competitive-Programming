2d62b1d3e807981e18312ddfc07d3f0eec2156fd
Aman Malakar <amanmalakar007@gmail.com>
2022-04-04 15:12:59 +0530

    Added Leetcode 31 to repo.
    1ms Java solution, better than 85% online Java submissions.

---
+++ LeetCode/31-Next-Permutation/Solution.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int i, min = Integer.MAX_VALUE, pos = -1;
+        boolean found = false;
+        for (i = nums.length - 2; i >= 0; i--) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] > nums[i] && nums[j] < min) {
+                    found = true;
+                    min = nums[j];
+                    pos = j;
+                }
+            }
+            if (found) {
+                int temp = nums[pos];
+                nums[pos] = nums[i];
+                nums[i] = temp;
+                sort(nums, i + 1);
+                break;
+            }
+        }
+        if (i == -1)
+            sort(nums, 0);
+    }
+
+    public void sort(int subArray[], int start) {
+        for (int i = start; i < subArray.length - 1; i++) {
+            for (int j = i + 1; j < subArray.length; j++) {
+                if (subArray[i] > subArray[j]) {
+                    int temp = subArray[i];
+                    subArray[i] = subArray[j];
+                    subArray[j] = temp;
+                }
+            }
+        }
+    }
+}
