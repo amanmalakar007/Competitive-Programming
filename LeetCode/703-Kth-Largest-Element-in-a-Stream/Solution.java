b6fd670ce1bd4519eb968e88842cd39d81ccb915
Aman Malakar <amanmalakar007@gmail.com>
2022-04-09 18:40:21 +0530

    Updated Solution for Leetcode 703
    No more TLE, 21ms, 52% better runtime, 82% better memory. :)

--- LeetCode/703-Kth-Largest-Element-in-a-Stream/Solution.java
+++ LeetCode/703-Kth-Largest-Element-in-a-Stream/Solution.java
@@ -1,18 +1,33 @@
 class KthLargest {
     int k;
-    ArrayList <Integer> list = new ArrayList <Integer> ();
+    ArrayList<Integer> list;
+    
     public KthLargest(int k, int[] nums) {
         this.k = k;
+        this.list = new ArrayList<Integer>();
         for (int i: nums) {
             this.list.add(i);
         }
+        Collections.sort(this.list);
     }
-
+	
     public int add(int val) {
-        this.list.add(val);
-        Collections.sort(this.list, Collections.reverseOrder());
-        System.out.println(list);
-        int ret = this.list.get((this.k) - 1);
-        return ret;
+        int pos = this.binarySearch(this.list,val,0,this.list.size()-1);
+        this.list.add(pos,val);
+        return this.list.get(this.list.size()-(this.k));
+    }
+	
+    private int binarySearch(List<Integer> list, int key, int low, int high)
+    {
+        int mid = low + ((high-low)/2);
+		if(low>high)
+            return mid;
+        int midVal = list.get(mid);
+        if (key == midVal)
+            return mid;
+        else if (key > midVal)
+            return (binarySearch(list,key,mid+1, high));
+        else
+            return (binarySearch(list,key,low,mid-1));
     }
 }
