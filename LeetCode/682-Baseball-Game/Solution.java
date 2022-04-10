c6ab8f9fd5398ce7bbdf15ef0818b3688da10c8b
Aman Malakar <amanmalakar007@gmail.com>
2022-04-10 13:26:11 +0530

    Added Leetcode 682 to repo.
    2ms solution, 97% better runtime, 93% better memory usage :)

---
+++ LeetCode/682-Baseball-Game/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int calPoints(String[] ops) {
+        List <Integer> scores = new ArrayList <Integer> ();
+        int i = 0;
+        for (String str: ops) {
+            if (str.equals("+")) {
+                scores.add(i, scores.get(i - 1) + scores.get(i - 2));
+                i++;
+            } else if (str.equals("D")) {
+                scores.add(i, 2 * (scores.get(i - 1)));
+                i++;
+            } else if (str.equals("C")) {
+                scores.remove(i - 1);
+                i--;
+            } else {
+                scores.add(i, Integer.parseInt(str));
+                i++;
+            }
+        }
+        int sum = 0;
+        for (int score: scores)
+            sum += score;
+        return sum;
+    }
+}
