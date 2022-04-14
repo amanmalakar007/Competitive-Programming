a9f2bb25de311ec32438ccfb9d56d2123e7276d7
Aman Malakar <amanmalakar007@gmail.com>
2022-04-14 17:15:30 +0530

    Added Leetcode 700 to repo.
    0ms solution, better runtime than 100% online Java submissions. Simple logic: just compare current curr.val with key, if same then return curr, else go either left or right.

---
+++ LeetCode/700-Search-in-a-Binary-Search-Tree/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        TreeNode curr = new TreeNode();
+        curr = root;
+        while (curr != null) {
+            if (val == curr.val)
+                return curr; 
+            else if (val > curr.val)
+                curr = curr.right;
+            else
+                curr = curr.left;
+        }
+        return null;
+    }
+}
